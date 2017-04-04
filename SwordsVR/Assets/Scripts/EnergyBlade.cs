850bcb6fc075ca60b9cf5ae10454a39d82d508ec
Rienzi Gokea <RGokea@gmail.com>
2017-04-03 20:32:32 -0400

    Set Up Disruptable Swords
    Made a quick script that disrupts the swords when hit a disruptor

---
+++ SwordsVR/Assets/Scripts/EnergyBlade.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyBlade : MonoBehaviour {
+
+	public bool disrupted;
+
+	public float bladeLength = 2.5f;
+	public float disruptTime = 1.5f;
+
+
+	private CapsuleCollider cc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Disruptor") {
+			StartCoroutine(DisruptBlade());
+		}
+	}
+
+	private IEnumerator DisruptBlade() {
+		float lerpVal = 1;
+		disrupted = true;
+		while (lerpVal > 0) {
+			transform.localPosition = Vector3.Lerp(new Vector3(0, .8f, 0), new Vector3(0, 3.5f, 0), lerpVal);
+			transform.localScale = Vector3.Lerp(new Vector3(.5f, .1f, .5f), new Vector3(.5f, 2.5f, .5f), lerpVal);
+
+			lerpVal -=  3 * Time.deltaTime;
+
+			yield return null;
+		}
+	}
+
+
+}
---
+++ SwordsVR/Assets/Scripts/KeyboardControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardControl : MonoBehaviour {
+
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 velocity = new Vector3(0, 0, 0);
+
+		if (Input.GetKey("w"))
+			velocity.z += 1;
+		if (Input.GetKey("s"))
+			velocity.z -= 1;
+		if (Input.GetKey("a"))
+			velocity.x -= 1;
+		if (Input.GetKey("d"))
+			velocity.x += 1;
+
+		rb.velocity = velocity;
+
+	}
+}
