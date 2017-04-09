4810707329741802c16687ab80fb90e681973f84
Rienzi Gokea <RGokea@gmail.com>
2017-04-08 23:03:34 -0400

    Started Up On Partial Sword Disruption
    Began the system to make the sword active and only be partially
    disrupted when hitting another sword in defense mode.

--- SwordsVR/Assets/Scripts/EnergyBlade.cs
+++ SwordsVR/Assets/Scripts/EnergyBlade.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class EnergyBlade : MonoBehaviour {
 
-	public bool disrupted;
+	// a testing variable so that I can control two swords with different keys
+	public bool testSword;
 
-	public float bladeLength = 2.5f;
+	public float basicBladeLength = 2.5f;
 	public float disruptTime = 1.5f;
 
-
 	private CapsuleCollider cc;
+	private Material mat;
+	private Color regColor;
 
-	public float timeToRegen;
+	private bool attackActive;
+
+	private bool disrupted;
+	private float timeToRegen;
+
+	//This is the latest energy blade collided with so we don't have to grab it every frame.  It gets reset when you exit the collider
+	private EnergyBlade otherBlade;
 
 	// Use this for initialization
 	void Start () {
+
+		mat = GetComponent<MeshRenderer>().material;
+		regColor = mat.color;
+
 		timeToRegen = disruptTime;
 	}
 	
@@ -24,6 +36,15 @@ public class EnergyBlade : MonoBehaviour {
 
 		if (disrupted)
 			UpdateRegen();
+
+		if (Input.GetKey(KeyCode.Space) && !testSword) {
+			mat.color = Color.red;
+			attackActive = true;
+		}
+		else { 
+			mat.color = regColor;
+			attackActive = false;
+		}
 
 	}
 
@@ -37,9 +58,23 @@ public class EnergyBlade : MonoBehaviour {
 	}
 
  
-  void OnCollisionEnter(Collision collision) {
-    print("test");
-  }
+	void OnCollisionEnter(Collision collision) {
+    	
+		if (collision.collider.gameObject.tag.Equals("EnergyBlade")) {
+			EnergyBlade blade = collision.collider.gameObject.GetComponent<EnergyBlade>();
+			if (blade != null) {
+				print("test");
+			}
+			else {
+				print("Error: Blade Collided with something tagged as 'EnergyBlade' but didn't have an EnergyBlade component script attached");
+			}
+		}
+
+	}
+
+	void OnCollisionExit(Collision collision) {
+	
+	}
 
 
 	void OnTriggerEnter(Collider col) {
@@ -60,7 +95,7 @@ public class EnergyBlade : MonoBehaviour {
 		Vector3 startPos = transform.localPosition;
 		Vector3 startSize = transform.localScale;
 		while (lerpVal > 0) {
-			transform.localPosition = Vector3.Lerp(new Vector3(0, .8f, 0), startPos, lerpVal);
+			transform.localPosition = Vector3.Lerp(new Vector3(0, .95f, 0), startPos, lerpVal);
 			transform.localScale = Vector3.Lerp(new Vector3(.5f, .1f, .5f), startSize, lerpVal);
 
 			lerpVal -=  15 * Time.deltaTime;
@@ -68,24 +103,34 @@ public class EnergyBlade : MonoBehaviour {
 			yield return null;
 		}
 
-		transform.localPosition = new Vector3(0, 0.8f, 0);
+		transform.localPosition = new Vector3(0, .95f, 0);
 		transform.localScale = new Vector3(.5f, .1f, .5f);
 		timeToRegen = disruptTime;
 	}
 
 	private IEnumerator RegenBlade() {
 		float lerpVal = 0;
+		Vector3 startPos = transform.localPosition;
+		Vector3 startSize = transform.localScale;
 		while (lerpVal < 1 && !disrupted) {
-			transform.localPosition = Vector3.Lerp(new Vector3(0, .8f, 0), new Vector3(0, 3.5f, 0), lerpVal);
-			transform.localScale = Vector3.Lerp(new Vector3(.5f, .1f, .5f), new Vector3(.5f, bladeLength, .5f), lerpVal);
+			transform.localPosition = Vector3.Lerp(startPos, new Vector3(0, 3.5f, 0), lerpVal);
+			transform.localScale = Vector3.Lerp(startSize, new Vector3(.5f, basicBladeLength, .5f), lerpVal);
 
 			lerpVal += 4 * Time.deltaTime;
 			yield return null;
 		}
 		if (!disrupted) {
 			transform.localPosition = new Vector3(0, 3.5f, 0);
-			transform.localScale = new Vector3(0.5f, bladeLength, 0.5f);
+			transform.localScale = new Vector3(0.5f, basicBladeLength, 0.5f);
 		}
+	}
+
+	public bool IsDisrupted() {
+		return disrupted;
+	}
+
+	public bool IsAttackActive() {
+		return attackActive;
 	}
 
 }
